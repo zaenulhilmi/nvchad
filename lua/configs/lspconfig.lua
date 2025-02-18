3d601d75e2ecc702cf827fe86c9b4237935dfb4c
Zaenul Hilmi <zaenulhilmi@gmail.com>
2025-02-18 23:39:45 +0700

    adding more config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -7,14 +7,37 @@ local lspconfig = require "lspconfig"
 local servers = { "html", "cssls", "gopls", "ts_ls", "lua_ls", "pyright", "rust_analyzer" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
+-- Define custom on_attach to include formatting keybind
+local function on_attach(client, bufnr)
+  -- Run NvChad's default on_attach
+  if nvlsp.on_attach then
+    nvlsp.on_attach(client, bufnr)
+  end
+
+  -- Enable formatting keybind if the LSP supports it
+  if client.server_capabilities.documentFormattingProvider then
+    vim.api.nvim_buf_set_keymap(
+      bufnr,
+      "n",
+      "<leader>fc",
+      "<cmd>lua vim.lsp.buf.format({ async = true })<CR>",
+      { noremap = true, silent = true }
+    )
+  end
+end
+
 -- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
+    on_attach = on_attach,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
   }
 end
+
+
+
+
 
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -16,7 +16,7 @@ return {
   	"nvim-treesitter/nvim-treesitter",
   	opts = {
   		ensure_installed = {
-  			"vim", "lua", "vimdoc", "html", "css", "go", "rust", "python", "typescript", "javascript"
+  			"vim", "lua", "vimdoc", "html", "css", "go", "rust", "python", "typescript", "javascript", "markdown"
   		},
   	},
   },
